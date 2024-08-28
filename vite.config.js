8cc4f0762afe1b5b2931248541bf166dc26dd1a0
Gbenga Oluwadahunsi <gbengaoluwadahunsi01@gmail.com>
2024-08-28 21:51:38 +0800

    Fix vercel files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/shoppingreact",
+//   plugins: [react()],
+// })
+
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
+
